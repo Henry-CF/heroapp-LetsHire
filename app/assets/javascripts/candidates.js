d7a3f735a10ca009aac81fdb658007b2bb99616f
Chunjie <czhu@vmware.com>
2013-05-09 18:14:22 +0800

    [PT 49581813] add file size limit both at client & server side [PT 49384979] increase the memory size value in letshire manifest

--- app/assets/javascripts/candidates.js
+++ app/assets/javascripts/candidates.js
@@ -10,4 +10,22 @@ $(function() {
         var url = "/positions/opening_options?selected_department_id=" + department_id;
         return select_wrapper.load(url);
     });
+
+    $('#candidate_resume').change(function(event) {
+        var maxsize = 10 * 1024 * 1024;
+
+        if ($.browser.msie) {
+            // microsoft ie
+            //
+            // It's not easy to achieve this functionality in IE, most likly,
+            // IE configuration does forbidden ActiveXObject. Anyway, we have
+            // done the server side file size limit mechanism.
+        } else {
+            // firefox, chrome
+            if (this.files[0].size > maxsize) {
+                alert('File size cannot be larger than 2M.');
+                $(this).attr('value', '');
+            }
+        }
+    });
 });
