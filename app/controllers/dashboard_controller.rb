c6f483884196ac9178e5664934997b6482a7689c
Chunjie <czhu@vmware.com>
2013-05-13 22:05:05 +0800

    [PT 49574045] job opening chart

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -18,5 +18,10 @@ class DashboardController < ApplicationController
       @interviews = Interview.all
     end
 
+    # data displayed in charts
+    @openings_created_by_me = Opening.openings_created_by_me(current_user.id).length
+    @openings_assigned_to_me = Opening.openings_assigned_to_me(current_user.id).length
+    @openings_without_interviewers = Opening.openings_without_interviewers.length
+
   end
 end
