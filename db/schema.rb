38d2427813299eb2a4b42fedcc6d70a93050ce73
Chunjie <czhu@vmware.com>
2013-05-14 23:19:42 +0800

    [PT 49430765] generalized solution for data storage layer
        stores both photos and resumes

--- db/schema.rb
+++ db/schema.rb
@@ -112,8 +112,8 @@ ActiveRecord::Schema.define(:version => 20130514025408) do
 
   create_table "photos", :force => true do |t|
     t.integer  "interview_id"
-    t.string   "photo_name"
-    t.string   "photo_path"
+    t.string   "name"
+    t.string   "path"
     t.text     "description"
     t.datetime "created_at",   :null => false
     t.datetime "updated_at",   :null => false
@@ -121,8 +121,8 @@ ActiveRecord::Schema.define(:version => 20130514025408) do
 
   create_table "resumes", :force => true do |t|
     t.integer  "candidate_id"
-    t.string   "resume_name"
-    t.string   "resume_path"
+    t.string   "name"
+    t.string   "path"
     t.datetime "created_at",   :null => false
     t.datetime "updated_at",   :null => false
   end
