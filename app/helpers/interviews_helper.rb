d49cf39107086003e2aa548462c75e787ab44cf7
Henry Chen <hchen@vmware.com>
2013-05-29 11:37:34 +0800

    several ability refine
    [50584785] should only 'schedule interviews' for candidates within interview loop
    [50711767] recruiter cannot select hiring manager in 'new job opening' page
    
    Signed-off-by: Henry Chen <hchen@vmware.com>

--- app/helpers/interviews_helper.rb
+++ app/helpers/interviews_helper.rb
@@ -1,5 +1,6 @@
 module InterviewsHelper
   def is_interviewer?(interviewers)
+    return true if current_user.admin?
     interviewers.each do |interviewer|
       return true if current_user.id == interviewer.user_id
     end
